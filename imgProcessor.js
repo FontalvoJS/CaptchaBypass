6986c4f41980ac421d29fc78e55086f6f10b12ef
Andrés Fontalvo <47583220+FontalvoJS@users.noreply.github.com>
2024-06-03 15:13:50 -0500

    Update imgProcessor.js
    Ahora con el servicio AWS Textract

--- imgProcessor.js
+++ imgProcessor.js
@@ -1,16 +1,16 @@
-const Jimp = require('jimp');
-const Tesseract = require('tesseract.js');
-
+const Jimp = require("jimp");
+const AWS = require("aws-sdk");
+const fs = require("fs");
 async function removeBorderNoise(imagePath) {
   const image = await Jimp.read(imagePath);
-  await image.writeAsync('test.png');
+  await image.writeAsync("Antes.png");
   const width = image.bitmap.width;
   const height = image.bitmap.height;
-  const borderSize = 7;  // Define el tamaño del borde que deseas verificar. Ajusta según tus necesidades.
+  const borderSize = 7; // Define el tamaño del borde que deseas verificar. Ajusta según tus necesidades.
 
   // Definir una función para determinar si un píxel es oscuro
   function isDarkPixel(red, green, blue) {
-    return red < 100 && green < 100 && blue < 100;  // Ajusta estos valores según tus necesidades.
+    return red < 100 && green < 100 && blue < 100; // Ajusta estos valores según tus necesidades.
   }
   // Convertir la imagen a escala de grises
   image.greyscale();
@@ -19,7 +19,7 @@ async function removeBorderNoise(imagePath) {
   const threshold = 128;
   image.scan(0, 0, image.bitmap.width, image.bitmap.height, (x, y, idx) => {
     const gray = Jimp.intToRGBA(image.getPixelColor(x, y)).r;
-    const newValue = gray < threshold ? 0x000000FF : 0xFFFFFFFF; // Black or White
+    const newValue = gray < threshold ? 0x000000ff : 0xffffffff; // Black or White
     image.setPixelColor(newValue, x, y);
   });
 
@@ -84,68 +84,79 @@ async function removeBorderNoise(imagePath) {
         }
       }
     }
-
-  }
+  };
   await deleteBorders();
 
-  image.convolute([
-    [1, 1, 1]
-  ]).convolute([[1, 1], [1, 1]])
+  image.convolute([[1, 1, 1]]).convolute([
+    [1, 1],
+    [1, 1],
+  ]);
   await erode(image, [[1, 1, 1]]);
 
-  await applyMedianFilter(image, [[1, 1, 1], [1, 1, 1], [1, 1, 1],]);
+  await applyMedianFilter(image, [
+    [1, 1, 1],
+    [1, 1, 1],
+    [1, 1, 1],
+  ]);
 
   // FILTROS - MODIFICACIONES
 
-  await image.writeAsync('testeado.png');
-  await Tesseract.recognize(
-    'testeado.png',
-    'eng', // idioma, en este caso inglés. Cambia según tu necesidad.
-  ).then(({ data: { text } }) => {
-    console.log(text); // Aquí se muestra el texto reconocido
-  }).catch(err => {
-    console.error('Error:', err);
-  });
-  await new Promise(resolve => setTimeout(resolve, 5000));
+  await image.writeAsync("Despues.png");
+  awsTextract("Despues.png");
+  await new Promise((resolve) => setTimeout(resolve, 5000));
 }
 async function applyMedianFilter(image, structElement) {
   const offsetX = Math.floor(structElement[0].length / 2);
   const offsetY = Math.floor(structElement.length / 2);
 
-  // Aplicar el filtro mediana
+  // filtro mediana
   image.scan(0, 0, image.bitmap.width, image.bitmap.height, (x, y, idx) => {
     const surroundingPixels = [];
 
     for (let i = -offsetY; i <= offsetY; i++) {
       for (let j = -offsetX; j <= offsetX; j++) {
-        if (structElement[i + offsetY][j + offsetX] &&
-          image.bitmap.width > x + j && x + j >= 0 &&
-          image.bitmap.height > y + i && y + i >= 0) {
-          surroundingPixels.push(Jimp.intToRGBA(image.getPixelColor(x + j, y + i)).r);
+        if (
+          structElement[i + offsetY][j + offsetX] &&
+          image.bitmap.width > x + j &&
+          x + j >= 0 &&
+          image.bitmap.height > y + i &&
+          y + i >= 0
+        ) {
+          surroundingPixels.push(
+            Jimp.intToRGBA(image.getPixelColor(x + j, y + i)).r
+          );
         }
       }
     }
 
     surroundingPixels.sort((a, b) => a - b); // Ordenando
 
-    const medianValue = surroundingPixels[Math.floor(surroundingPixels.length / 2)];
+    const medianValue =
+      surroundingPixels[Math.floor(surroundingPixels.length / 2)];
     const newValue = Jimp.rgbaToInt(medianValue, medianValue, medianValue, 255);
     image.setPixelColor(newValue, x, y);
   });
-};
+}
 function erode(image, se) {
-  const seCenter = { x: Math.floor(se[0].length / 2), y: Math.floor(se.length / 2) };
+  const seCenter = {
+    x: Math.floor(se[0].length / 2),
+    y: Math.floor(se.length / 2),
+  };
 
   image.scan(0, 0, image.bitmap.width, image.bitmap.height, (x, y, idx) => {
     let erodePixel = true;
 
-    // Verificar cada pixel del elemento estructurante
     for (let i = 0; i < se.length; i++) {
       for (let j = 0; j < se[i].length; j++) {
         const xi = x - seCenter.x + i;
         const yj = y - seCenter.y + j;
 
-        if (xi >= 0 && xi < image.bitmap.width && yj >= 0 && yj < image.bitmap.height) {
+        if (
+          xi >= 0 &&
+          xi < image.bitmap.width &&
+          yj >= 0 &&
+          yj < image.bitmap.height
+        ) {
           const pixelColor = Jimp.intToRGBA(image.getPixelColor(xi, yj));
           if (se[i][j] === 1 && pixelColor.r === 255) {
             erodePixel = false;
@@ -154,19 +165,92 @@ function erode(image, se) {
       }
     }
 
-    // Si erodePixel es verdadero, establecer el pixel central en negro
     if (erodePixel) {
-      image.setPixelColor(0x000000FF, x, y);
+      image.setPixelColor(0x000000ff, x, y);
     }
   });
 
   return image;
 }
-async function main() {
-  let desde = 0;
-  let hasta = 100;
-  for (let i = desde; i <= hasta; i++) {
-    await removeBorderNoise('http://serviciospub.sic.gov.co/Sic2/Tramites/Radicacion/Radicacion/Librerias/securimage/securimage_show.php?sid=4674619f7bfc3d1c83a5576e838f8968');
+async function awsTextract(image) {
+  try {
+    console.clear();
+    console.log("======================================");
+    console.log("Analizando imagen...");
+    console.log("======================================");
+    AWS.config.update({
+      region: process.env.AWS_REGION,
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    });
+
+    uploadToS3(image);
+
+    const textract = new AWS.Textract();
+    const bucketName = process.env.AWS_BUCKET_NAME;
+
+    const params = {
+      Document: {
+        S3Object: {
+          Bucket: bucketName,
+          Name: image,
+        },
+      },
+    };
+
+    textract.detectDocumentText(params, (err, data) => {
+      if (data) {
+        console.log("BYPASS: ");
+        console.log("-------");
+        for (let key in data.Blocks) {
+          if (
+            data.Blocks[key].Text?.length > 0 
+            && data.Blocks[key].BlockType === "LINE"
+          ) {
+            
+            console.log(
+              "Codigo: [",
+              data.Blocks[key].Text,
+              "] | Formato:",
+              data.Blocks[key].BlockType
+            );
+          }
+        }
+        console.log("======================================");
+      }
+    });
+  } catch (error) {
+    console.log("awsTextract: ", error);
   }
+}
+
+function uploadToS3(image) {
+  try {
+    const s3 = new AWS.S3();
+    const params = {
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: image,
+      Body: fs.readFileSync(image),
+    };
+    s3.upload(params, (err, data) => {
+      if (data) {
+        console.log("Imagen almacenada correctamente en S3");
+        console.log("======================================");
+      }
+    });
+  } catch (error) {
+    console.log("uploadToS3: ", error);
+  }
+}
+async function main() {
+  console.log("By: FontalvoJS | https://github.com/FontalvoJS");
+  let from = 0;
+  let until = 10;
+  for (let i = from; i <= until; i++) {
+    await removeBorderNoise(
+      "http://serviciospub.sic.gov.co/Sic2/Tramites/Radicacion/Radicacion/Librerias/securimage/securimage_show.php?sid=4674619f7bfc3d1c83a5576e838f8968"
+    );
+  }
+  process.exit(0);
 }
 main();
